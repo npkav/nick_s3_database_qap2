88a46b9b123747708d6abb7668746a295c12011d
Nick <174647283+npkav@users.noreply.github.com>
2025-02-09 11:23:04 -0330

    init commit

---
+++ problem_1_enrollment_system/insert_data.sql
@@ -0,0 +1,11 @@
+-- Nickolas Kavanagh - SD12
+-- Semester 3 - Database Programming
+-- 2025/02/03 - 2025/02/10
+
+
+-- Problem 1: University Course Enrollment System
+
+-- Insert Data:
+
+-- Insert at least 5 students, 4 professors, and 3 courses.
+-- Enroll multiple students in different courses, making sure there are at least 5 enrollments in total.
